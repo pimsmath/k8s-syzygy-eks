509a4ea7c35fadd2ad0b7bc9393d02cef16fbf9d
Ian Allison <iana@pims.math.ca>
2020-01-21 13:17:03 -0800

    Drop requirement for availability zones to 2
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- main.tf
+++ main.tf
@@ -81,8 +81,8 @@ module "vpc" {
   name                 = "eks-vpc"
   cidr                 = "10.1.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
-  public_subnets       = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets       = ["10.1.101.0/24", "10.1.102.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
