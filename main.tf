5ec403331cb82f3e9df4bc128b52bb8c95be95e1
EC2 Default User <ec2-user@ip-172-31-1-243.ca-central-1.compute.internal>
2020-09-05 02:21:07 +0000

    Adding IRSA
    Signed-off-by: EC2 Default User <ec2-user@ip-172-31-1-243.ca-central-1.compute.internal>

--- main.tf
+++ main.tf
@@ -30,11 +30,21 @@ provider "template" {
   version = "~> 2.1"
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
 data "aws_availability_zones" "available" {
 }
 
 locals {
   cluster_name = "syzygy-eks-${random_string.suffix.result}"
+  k8s_service_account_namespace = "kube-system"
+  k8s_service_account_name      = "cluster-autoscaler-aws-cluster-autoscaler"
 }
 
 resource "random_string" "suffix" {
@@ -118,6 +128,8 @@ module "eks" {
   }
 
   vpc_id = module.vpc.vpc_id
+
+  enable_irsa   = true
 
   worker_groups = [
     {
